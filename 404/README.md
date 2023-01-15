4fcfa6e5502e2bdaff0fea095de74c4e6db87ecd
Laura <l.kocetkova.mv@gmail.com>
2023-01-15 19:20:00 +0200

    init

---
+++ 404/README.md
@@ -0,0 +1,4 @@
+404 Page Animation Using GSAP
+
+
+https://www.youtube.com/watch?v=Y_aqUJCoyYI&list=PLbuY90yp4Qpb3zIidm7BCfmMy7N1DmRbp&index=49&t=10s
---
+++ script.js
@@ -0,0 +1,24 @@
+gsap.from(".navbar > div", 1.6, {
+    opacity: 0,
+    y: 60,
+    ease: Expo.easeInOut,
+    delay: 2.4,
+}),
+gsap.from(".site-logo", 1.6, {
+    opacity: 0,
+    y: 40,
+    ease: Expo.easeInOut,
+    delay: 2.4,
+});
+gsap.from(".header > span", 1.5, {
+    top: "-100vh",
+    ease: "bounce.out",
+    delay: 1,
+    stagger: 0.2,
+});
+gsap.to(".footer span", 0.4, {   
+    y: -40,
+    ease: Expo.easeInOut,
+    delay: 1,
+    stager: 2.4,
+});
